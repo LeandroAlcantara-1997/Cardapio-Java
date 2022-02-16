83802d31658cf004b967cabaf36392d0ff43ebd9
Leandro Alcantara <leandro1997silva97@gmail.com>
2022-02-16 20:16:37 -0300

    Repositorio criado

---
+++ src/test/java/com/github/leandroalcantara_1997/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandroalcantara_1997.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
